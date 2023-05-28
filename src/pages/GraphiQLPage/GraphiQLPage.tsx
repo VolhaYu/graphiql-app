7f3b7711c59b041742beda0879fd3e9caf397d30
Uladzimir <vl.barbikov@gmail.com>
2023-05-28 14:36:21 +0300

    fix: schema is visible

--- src/pages/GraphiQLPage/GraphiQLPage.tsx
+++ src/pages/GraphiQLPage/GraphiQLPage.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import './GraphiQLPage.scss';
-import { useAppSelector } from '../../store/hooks/redux';
 import GraphQLEditor from '../../components/GraphQLEditor/GraphQlEditor';
 import GraphQLResponse from '../../components/GraphQLResponse/GraphQLResponse';
 import GraphQLSchema from '../../components/GraphQLSchema/GraphQLSchema';
 
 function GraphiQlPage() {
-  const responseStatus = useAppSelector((state) => state.responseStatusSlice.ok);
-
   return (
     <main>
       <div className="graphiql">
         <GraphQLEditor />
         <GraphQLResponse />
-        {responseStatus ? <GraphQLSchema /> : null}
+        <GraphQLSchema />
       </div>
     </main>
   );
